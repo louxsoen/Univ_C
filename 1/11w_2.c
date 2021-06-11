955451dfe31338935722bf907cd2fedf9aed3ac2
Tihaba <32505983+Tihaba@users.noreply.github.com>
2021-06-11 21:15:18 +0900

    Update 11w_2.c

--- 1/11w_2.c
+++ 1/11w_2.c
@@ -12,5 +12,5 @@ int main()
     struct birthday t1 = { 1995, 7, 5 }; // 구조체 초기화
     printf("생년월일: %d년 %d월 %d일생\n", t1.year, t1.month, t1.day); // 출력
 
-    return 0; // 정상 종료
+    return 0; // 정상 종료 // 테스트ㅁㄴㅇ
 }
