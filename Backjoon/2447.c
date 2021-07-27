68dfa6deec112d1320ee4a1f24f922eec817a13f
iphone <73981769+louxsoen@users.noreply.github.com>
2021-07-27 19:47:07 +0900

    2447

---
+++ Backjoon/2447.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void fr();
+
+int main() 
+{
+	int num;
+	scanf("%d", &num);
+    for (int i = 0 ; i < num ; i++)
+    {
+        for (int j = 0 ; j < num ; j++)
+        fr(i, j, num);
+          puts("");
+    }
+
+}
+
+void fr(int i, int j, int num) 
+{
+    if ( (i/num) % 3 == 1 && (j/num) % 3 == 1 )     printf(" ");
+	else if ( num/3 == 0 )                          printf("*");
+	else                                            fr(i, j, num / 3);
+}
+
+// 3 3 3
