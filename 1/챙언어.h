4da4c99999f922af7c5b2a66d74bcd7839ac0ed4
iphone <73981769+louxsoen@users.noreply.github.com>
2021-09-24 23:42:09 +0900

    ㅊㅐㅇ언어

---
+++ 1/챙언어.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+typedef int 고대;
+typedef char 연대;
+#define 잔소리 512
+#define 챙아뭐라고 printf
+#define 설대 puts
+#define 챙이삐져또 return
+#define 채은 main
+#define 또챙 for
+#define 캠퍼스 sizeof
+#define 사랑해 stdin
+#define 말을해봐 fgets
